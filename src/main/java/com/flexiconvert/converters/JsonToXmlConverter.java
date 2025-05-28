2cd88d754bd8ce6beddcd00f05a2ef487070ddd4
Yash Jain <jain.yp@somaiya.edu>
2025-05-28 14:15:57 -0700

    Packaged the application

--- src/main/java/com/flexiconvert/converters/JsonToXmlConverter.java
+++ src/main/java/com/flexiconvert/converters/JsonToXmlConverter.java
@@ -2,10 +2,13 @@ package com.flexiconvert.converters;
 
 import com.flexiconvert.interfaces.FormatConverter;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class JsonToXmlConverter implements FormatConverter {
 
@@ -14,8 +17,19 @@ public class JsonToXmlConverter implements FormatConverter {
         ObjectMapper jsonMapper = new ObjectMapper();
         XmlMapper xmlMapper = new XmlMapper();
 
-        JsonNode jsonNode = jsonMapper.readTree(jsonFile);
-        String xml = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+        JsonNode original = jsonMapper.readTree(jsonFile);
+
+        // Wrap in a root object if it's an array
+        JsonNode wrapped;
+        if (original.isArray()) {
+            ObjectNode root = jsonMapper.createObjectNode();
+            root.set("record", original); // each element will become <record>
+            wrapped = root;
+        } else {
+            wrapped = original;
+        }
+
+        String xml = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(wrapped);
 
         File output = new File(jsonFile.getParent(), jsonFile.getName().replaceAll("(?i)\\.json$", ".xml"));
         try (FileWriter writer = new FileWriter(output)) {
